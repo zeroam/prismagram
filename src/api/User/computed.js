40dcb5b25fa655c0873393186af82e149c58035d
jayone <imdff0803@gmail.com>
2021-01-26 21:04:08 +0900

    graphql api - isSelf and isFollowing

---
+++ src/api/User/computed.js
@@ -0,0 +1,21 @@
+import { prisma } from "../../../generated/prisma-client";
+
+export default {
+  User: {
+    fullName: (parent) => {
+      return `${parent.firstName} ${parent.lastName}`;
+    },
+    isFollowing: (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      return prisma.$exists.user({
+        AND: [{ id: parentId }, { followers_some: { id: user.id } }],
+      });
+    },
+    isSelf: (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      return user.id === parentId;
+    },
+  },
+};
