7f15dcb1bd4a0c42a70a1bc6b9057e3716507777
jayone <imdff0803@gmail.com>
2021-02-01 21:48:49 +0900

    graphql api - Kill The Fragments

--- src/api/Post/seeFullPost/seeFullPost.js
+++ src/api/Post/seeFullPost/seeFullPost.js
@@ -1,11 +1,10 @@
 import { prisma } from "../../../../generated/prisma-client";
-import { FULL_POST_FRAGMENT } from "../../../fragment";
 
 export default {
   Query: {
     seeFullPost: async (_, args) => {
       const { id } = args;
-      return await prisma.post({ id }).$fragment(FULL_POST_FRAGMENT);
+      return await prisma.post({ id });
     },
   },
 };
--- src/fragment.js
+++ src/fragment.js
@@ -1,20 +1,7 @@
-export const COMMENT_FRAGMENT = `
-  id
-  text
-  user {
-    ${USER_FRAGMENT}
-  }
-`;
-
 export const USER_FRAGMENT = `
   id
   username
   avatar
-`;
-
-export const FILE_FRAGMENT = `
-  id
-  url
 `;
 
 export const MESSAGE_FRAGMENT = `
@@ -25,23 +12,6 @@ export const MESSAGE_FRAGMENT = `
   }
   from {
     ${USER_FRAGMENT}
-  }
-`;
-
-export const FULL_POST_FRAGMENT = `
-  fragment PostParts on Post {
-    id
-    location
-    caption
-    files {
-      ${FILE_FRAGMENT}
-    }
-    comments {
-      ${COMMENT_FRAGMENT}
-    }
-    user {
-      ${USER_FRAGMENT}
-    }
   }
 `;
 
